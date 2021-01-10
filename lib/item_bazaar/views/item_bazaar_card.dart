a60de7f87306e4a66e2fc7adf97b1df5190a087a
Kim Ward <wardy484@gmail.com>
2021-01-10 11:21:14 +0000

    Added linking to auction house listing from bazaar on player

--- lib/item_bazaar/views/item_bazaar_card.dart
+++ lib/item_bazaar/views/item_bazaar_card.dart
@@ -1,10 +1,14 @@
+import 'package:eden_xi_tools/eden/player/repository/player_repository.dart';
 import 'package:eden_xi_tools/extensions/int.dart';
 import 'package:eden_xi_tools/eden/items/entities/bazaar_item/bazaar_item.dart';
+import 'package:eden_xi_tools/player_show/pages/player_show_page.dart';
 import 'package:eden_xi_tools/styles/spacing.dart';
 import 'package:eden_xi_tools/styles/text_styles.dart';
 
 import 'package:eden_xi_tools/widgets/online_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kiwi/kiwi.dart';
 
 class ItemBazaarCard extends StatelessWidget {
   final BazaarItem item;
@@ -13,34 +17,60 @@ class ItemBazaarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: BoxPadding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    OnlineIndicator(onlineFlag: item.onlineFlag),
-                    SizedBox(width: 8),
-                    Text(
-                      item.charname,
-                      style: SubHeading,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: BoxPaddingRight,
-                  child: Text(item.bazaar.toGil()),
-                ),
-              ],
-            ),
-          ],
+    return GestureDetector(
+      onTap: () => _navigateToPlayer(item.charname, context),
+      child: Card(
+        child: Padding(
+          padding: BoxPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      OnlineIndicator(onlineFlag: item.onlineFlag),
+                      SizedBox(width: 8),
+                      Text(
+                        item.charname,
+                        style: SubHeading,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: BoxPaddingRight,
+                    child: Text(item.bazaar.toGil()),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _navigateToPlayer(String playerName, context) async {
+    var playerRepository = KiwiContainer().resolve<PlayerRepository>();
+    var players = await playerRepository.search(playerName, 0, 1);
+
+    if (players.total > 0) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PlayerShowPage(
+            playerResult: players.items.first,
+          ),
+        ),
+      );
+    } else {
+      Fluttertoast.showToast(
+        msg: "Player not found...",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.grey[800],
+      );
+    }
   }
 }
--- lib/player_show/views/tabs/player_show_bazaar.dart
+++ lib/player_show/views/tabs/player_show_bazaar.dart
@@ -1,8 +1,12 @@
+import 'package:eden_xi_tools/eden/items/repositories/ItemRepository.dart';
 import 'package:eden_xi_tools/eden/player/entities/player_bazaar_item/player_bazaar_item.dart';
+import 'package:eden_xi_tools/item_show/pages/item_show_page.dart';
 import 'package:eden_xi_tools/styles/spacing.dart';
 import 'package:eden_xi_tools/styles/text_styles.dart';
 import 'package:eden_xi_tools/widgets/centered_message.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kiwi/kiwi.dart';
 import 'package:recase/recase.dart';
 import 'package:eden_xi_tools/extensions/int.dart';
 
@@ -37,22 +41,28 @@ class PlayerShowBazaar extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            name.titleCase,
-                            style: SubHeading,
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: BoxPaddingRight,
-                        child: Text(item.bazaar.toGil()),
-                      ),
-                    ],
+                  GestureDetector(
+                    onTap: () => _navigateToItem(
+                      name.titleCase.replaceAll("+1", ""),
+                      context,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              name.titleCase,
+                              style: SubHeading,
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: BoxPaddingRight,
+                          child: Text(item.bazaar.toGil()),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -61,5 +71,28 @@ class PlayerShowBazaar extends StatelessWidget {
         },
       ),
     );
+  }
+
+  _navigateToItem(String itemName, context) async {
+    var itemRepository = KiwiContainer().resolve<ItemRepository>();
+    var items = await itemRepository.search(itemName, 0, 1);
+
+    if (items.total > 0) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ItemShowPage(
+            item: items.items.first,
+          ),
+        ),
+      );
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Unable to find item on auction house."),
+          duration: Duration(seconds: 4),
+        ),
+      );
+    }
   }
 }
