b2e58efdd2a18d21bdf705c3ff9f58b83c55f3f8
Kim Ward <wardy484@gmail.com>
2021-01-18 22:50:03 +0000

    Potential fix for refreshing server status?

--- lib/dashboard/pages/dashboard_page.dart
+++ lib/dashboard/pages/dashboard_page.dart
@@ -33,76 +33,91 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SwipableScaffold(
-      resetOnPop: true,
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Image.asset(
-              "icon_simple.png",
-              color: Colors.white,
-              width: 35,
-              height: 35,
+    return BlocListener<ServerStatusBloc, ServerStatusState>(
+      listener: (context, state) {
+        state.maybeWhen(
+          offline: () {
+            _refreshCompleter?.complete();
+            _refreshCompleter = Completer();
+          },
+          online: (players) {
+            _refreshCompleter?.complete();
+            _refreshCompleter = Completer();
+          },
+          orElse: () => {},
+        );
+      },
+      child: SwipableScaffold(
+        resetOnPop: true,
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Image.asset(
+                "icon_simple.png",
+                color: Colors.white,
+                width: 35,
+                height: 35,
+              ),
+              SizedBox(width: 14),
+              Text("Eden XI Tools"),
+            ],
+          ),
+          actions: [
+            FlatButton(
+              minWidth: 60,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(),
+                  ),
+                );
+              },
+              child: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
             ),
-            SizedBox(width: 14),
-            Text("Eden XI Tools"),
           ],
         ),
-        actions: [
-          FlatButton(
-            minWidth: 60,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SettingsPage(),
-                ),
-              );
+        pages: [
+          RefreshIndicator(
+            onRefresh: () async {
+              _serverStatusBloc.add(ServerStatusEvent.fetched());
             },
-            child: Icon(
-              Icons.settings,
-              color: Colors.white,
+            child: ListView(
+              children: [
+                SizedBox(height: 8),
+                DashboardServerStatus(),
+                DashboardFavouritePlayersCard(),
+                DashboardFavouriteItemsCard(),
+                SizedBox(height: 16),
+              ],
             ),
+          ),
+          ItemSearchTab(),
+          PlayerSearchTab(),
+          YellsTab(),
+        ],
+        tabs: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gavel_rounded),
+            label: 'Item Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Player Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            label: 'Yells',
           ),
         ],
       ),
-      pages: [
-        RefreshIndicator(
-          onRefresh: () async {
-            _serverStatusBloc.add(ServerStatusEvent.fetched());
-          },
-          child: ListView(
-            children: [
-              SizedBox(height: 8),
-              DashboardServerStatus(),
-              DashboardFavouritePlayersCard(),
-              DashboardFavouriteItemsCard(),
-              SizedBox(height: 16),
-            ],
-          ),
-        ),
-        ItemSearchTab(),
-        PlayerSearchTab(),
-        YellsTab(),
-      ],
-      tabs: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Dashboard',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.gavel_rounded),
-          label: 'Item Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.people),
-          label: 'Player Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat_bubble),
-          label: 'Yells',
-        ),
-      ],
     );
   }
 }
