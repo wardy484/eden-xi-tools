5f78bf526f11a278756e8a39dc1760f0e8df143e
Kim Ward <wardy484@gmail.com>
2021-01-24 14:05:45 +0000

    Remove commented code

--- lib/dashboard/pages/dashboard_page.dart
+++ lib/dashboard/pages/dashboard_page.dart
@@ -13,6 +13,7 @@ import 'package:hooks_riverpod/all.dart';
 
 class DashboardPage extends HookWidget {
   const DashboardPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Future.delayed(
@@ -95,98 +96,3 @@ class DashboardPage extends HookWidget {
     );
   }
 }
-
-// class DashboardPage extends StatefulWidget {
-//   const DashboardPage({Key key}) : super(key: key);
-
-//   @override
-//   _DashboardPageState createState() => _DashboardPageState();
-// }
-
-// class _DashboardPageState extends State<DashboardPage> {
-//   Completer<void> _refreshCompleter;
-//   ServerStatusBloc _serverStatusBloc;
-
-//   @override
-//   void initState() {
-//     _refreshCompleter = Completer<void>();
-//     _serverStatusBloc = BlocProvider.of<ServerStatusBloc>(context);
-
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SwipableScaffold(
-//       resetOnPop: true,
-//       appBar: AppBar(
-//         title: Row(
-//           children: [
-//             Image.asset(
-//               "icon_simple.png",
-//               color: Colors.white,
-//               width: 35,
-//               height: 35,
-//             ),
-//             SizedBox(width: 14),
-//             Text("Eden XI Tools"),
-//           ],
-//         ),
-//         actions: [
-//           FlatButton(
-//             minWidth: 60,
-//             onPressed: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (context) => SettingsPage(),
-//                 ),
-//               );
-//             },
-//             child: Icon(
-//               Icons.settings,
-//               color: Colors.white,
-//             ),
-//           ),
-//         ],
-//       ),
-//       pages: [
-//         RefreshIndicator(
-//           onRefresh: () async {
-//             _serverStatusBloc.add(ServerStatusEvent.fetched());
-//           },
-//           child: ListView(
-//             children: [
-//               SizedBox(height: 8),
-//               DashboardServerStatus(),
-//               DashboardFavouritePlayersCard(),
-//               DashboardFavouriteItemsCard(),
-//               SizedBox(height: 16),
-//             ],
-//           ),
-//         ),
-//         ItemSearchTab(),
-//         PlayerSearchTab(),
-//         YellsTab(),
-//       ],
-//       tabs: const <BottomNavigationBarItem>[
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.home),
-//           label: 'Dashboard',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.gavel_rounded),
-//           label: 'Item Search',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.people),
-//           label: 'Player Search',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.chat_bubble),
-//           label: 'Yells',
-//         ),
-//       ],
-//     );
-//   }
-// }
