a0ed5b79de552ebee530a3b94f7e8ac9ea3add0c
Kim Ward <wardy484@gmail.com>
2022-07-09 22:38:58 +0100

    refactored player show page

--- lib/player_show/pages/player_show_page.dart
+++ lib/player_show/pages/player_show_page.dart
@@ -1,8 +1,14 @@
 import 'dart:async';
+import 'package:eden_xi_tools/item_show/views/item_show_scaffold.dart';
+import 'package:eden_xi_tools/player_favourites/views/player_favourite_button.dart';
 import 'package:eden_xi_tools/player_show/player_show_notifier.dart';
 import 'package:eden_xi_tools/player_show/player_show_state.dart';
-import 'package:eden_xi_tools/player_show/views/states/player_show_loading_state.dart';
-import 'package:eden_xi_tools/player_show/views/states/player_show_success_state.dart';
+import 'package:eden_xi_tools/player_show/views/player_show_navigation_bar.dart';
+import 'package:eden_xi_tools/player_show/views/widgets/player_show_header.dart';
+import 'package:eden_xi_tools/player_show/views/tabs/player_show_auction_house.dart';
+import 'package:eden_xi_tools/player_show/views/tabs/player_show_bazaar.dart';
+import 'package:eden_xi_tools/player_show/views/tabs/player_show_details.dart';
+import 'package:eden_xi_tools/widgets/swipable_pages/swipable_pages.dart';
 import 'package:eden_xi_tools_api/eden_xi_tools_api.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,6 +27,7 @@ class PlayerShowPage extends ConsumerStatefulWidget {
 
 class _PlayerShowPageState extends ConsumerState<PlayerShowPage> {
   late Completer<void> _refreshCompleter;
+  int _selectedPageIndex = 0;
 
   @override
   void initState() {
@@ -33,6 +40,62 @@ class _PlayerShowPageState extends ConsumerState<PlayerShowPage> {
 
   @override
   Widget build(BuildContext context) {
+    _registerLoadingListener();
+
+    return ItemShowScaffold(
+        header: PlayerShowHeader(playerResult: widget.playerResult),
+        body: ref.watch(playerShowProvider).maybeWhen(
+              loaded: (
+                playerResult,
+                player,
+                equipment,
+                auctionHouseItems,
+                bazaarItems,
+                crafts,
+              ) {
+                return SwipablePages(
+                  index: _selectedPageIndex,
+                  onSwipe: _onPageNavigation,
+                  pages: [
+                    PlayerShowDetails(
+                      player: player,
+                      crafts: crafts,
+                      equipment: equipment,
+                      onRefresh: _refreshPage,
+                    ),
+                    PlayerShowAuctionHouse(
+                      items: auctionHouseItems,
+                      onRefresh: _refreshPage,
+                    ),
+                    PlayerShowBazaar(
+                      items: bazaarItems,
+                      onRefresh: _refreshPage,
+                    ),
+                  ],
+                );
+              },
+              orElse: () => Center(child: CircularProgressIndicator()),
+            ),
+        bottomNavigationBar: PlayerShowNavigationBar(
+          currentIndex: _selectedPageIndex,
+          onTap: _onPageNavigation,
+        ),
+        actions: [
+          PlayerFavouriteButton(player: widget.playerResult),
+        ]);
+  }
+
+  Future<void> _refreshPage() {
+    ref.watch(playerShowProvider.notifier).getPlayer(widget.playerResult);
+
+    return _refreshCompleter.future;
+  }
+
+  void _onPageNavigation(int index) {
+    setState(() => _selectedPageIndex = index);
+  }
+
+  void _registerLoadingListener() {
     ref.listen(playerShowProvider,
         (PlayerShowState? previous, PlayerShowState next) {
       previous?.whenOrNull(
@@ -53,38 +116,5 @@ class _PlayerShowPageState extends ConsumerState<PlayerShowPage> {
         },
       );
     });
-
-    return ref.watch(playerShowProvider).maybeWhen(
-          loaded: (
-            playerResult,
-            player,
-            equipment,
-            auctionHouseItems,
-            bazaarItems,
-            crafts,
-          ) =>
-              PlayerShowSuccessState(
-            playerResult: playerResult,
-            player: player,
-            equipment: equipment,
-            auctionHouseItems: auctionHouseItems,
-            bazaarItems: bazaarItems,
-            crafts: crafts,
-            onRefreshPressed: _refreshPage,
-          ),
-          orElse: () =>
-              PlayerShowLoadingState(playerResult: widget.playerResult),
-        );
-  }
-
-  Future<void> _refreshPage() {
-    ref.watch(playerShowProvider.notifier).getPlayer(widget.playerResult);
-
-    return _refreshCompleter.future;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
