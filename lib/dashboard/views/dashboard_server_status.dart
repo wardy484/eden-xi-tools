1eea36e9dd6cc5c5cc22ba1084aaa334ddf51554
Kim Ward <wardy484@gmail.com>
2022-07-09 20:07:02 +0100

    Moved server status widget

--- lib/dashboard/views/dashboard_server_status.dart
+++ lib/dashboard/views/dashboard_server_status.dart
@@ -1,5 +1,5 @@
 import 'package:eden_xi_tools/server_status/server_status_notifier.dart';
-import 'package:eden_xi_tools/widgets/server_status_indicator.dart';
+import 'package:eden_xi_tools/server_status/view/server_status_indicator.dart';
 import 'package:eden_xi_tools_api/eden_xi_tools_api.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
